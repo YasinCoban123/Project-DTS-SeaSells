0835a82a2fae2fb87e12847c2f915292f3d08dbe
AmineArr <Aminearram2007@gmail.com>
2026-03-10 11:26:27 +0100

    db

--- Template/Project/DataAccess/AccountAcces.cs
+++ Template/Project/DataAccess/AccountAcces.cs
@@ -1,37 +1,56 @@
 using Npgsql;
 using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
-
-public class UserAccountsAccess
+public class AccountsAccess
 {
-    private NpgsqlConnection _connection = new NpgsqlConnection(
-        "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=..."
-    );
-
+    private readonly string _connectionString;
     private string Table = "Account";
+
+    public AccountsAccess()
+    {
+        // Lees connection string uit appsettings.json
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
 
     public void Write(AccountModel account)
     {
+        using var _connection = new NpgsqlConnection(_connectionString);
+        _connection.Open();
+
         string sql = $"INSERT INTO {Table} (name, email, password) VALUES (@Name, @Email, @Password)";
         _connection.Execute(sql, account);
     }
 
     public AccountModel GetByEmail(string email)
     {
+        using var _connection = new NpgsqlConnection(_connectionString);
+        _connection.Open();
+
         string sql = $"SELECT * FROM {Table} WHERE email = @Email";
         return _connection.QueryFirstOrDefault<AccountModel>(sql, new { Email = email });
     }
 
     public void Update(AccountModel account)
     {
+        using var _connection = new NpgsqlConnection(_connectionString);
+        _connection.Open();
+
         string sql = $"UPDATE {Table} SET email = @Email, password = @Password, name = @Name WHERE UserId = @UserId";
         _connection.Execute(sql, account);
     }
 
     public void Delete(AccountModel account)
     {
+        using var _connection = new NpgsqlConnection(_connectionString);
+        _connection.Open();
+
         string sql = $"DELETE FROM {Table} WHERE UserId = @UserId";
         _connection.Execute(sql, account);
     }
-
 }
